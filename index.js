2bce3c9cb24e334b40d5231e9b60e254d4dbdaea
lucas <parra.lucas@laposte.net>
2024-01-11 15:57:10 +0100

    html,css,et dubut du js

---
+++ index.js
@@ -0,0 +1,18 @@
+let submit = document.getElementById("reserver")
+const ajouter = document.getElementById("plus")
+const date1 = document.getElementById("date1")
+const date2 = document.getElementById("date2")
+const date3 = document.getElementById("date3")
+let  date = [
+ date1, date2, date3
+]
+submit.addEventListener("click", enregistrerForm)
+
+function enregistrerForm(){
+
+    
+
+
+    
+
+}
