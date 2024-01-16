0eedd5edf047acdd5f386f62f9194e4516e695e7
lucas <parra.lucas@laposte.net>
2024-01-16 09:49:45 +0100

    ajouter des champs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dates = [
     document.getElementById("date2"),
     document.getElementById("date3"),
 ]
+
 const places = document.getElementById("nombrePlaces")
 const btn = document.getElementById("plus");
 const originalForm = document.getElementById("reservationForm"); 
@@ -14,7 +15,7 @@ submit.addEventListener("click", enregistrerForm);
 
 function ajouterChamps() {
 
-    
+    addElement()
 
 }
 
@@ -24,11 +25,22 @@ function enregistrerForm() {
     console.log(dates)
 }
 
-function addElement(){
-
-
-
-const newInput = document.createElement("input")
-document.body.insertBefore(newInput, places)
+function addElement() {
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `
+        <div>
+            <label for="date">Choisissez une date :</label>
+            <select name="date" id="date">
+                <option value="date1" id="date1">10 fevrier 2024</option>
+                <option value="date2" id="date2">15 fevrier 2024</option>
+                <option value="date3" id="date3">20 fevrier 2024</option>
+            </select>
+            <br>
+            <label for="nombrePlaces">Nombre de places :</label>
+            <input type="number" name="nombrePlaces" id="nombrePlaces" min="1" required>
+        </div>
+    `;
+    originalForm.insertBefore(newDiv, btn);
 }
+
 
